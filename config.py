7d12c41b9f1fa87c2ea79042fbb357082d950b64
aswinxd <155294690+aswinxd@users.noreply.github.com>
2024-01-17 16:10:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,36 @@
-# config.py
-API_ID = '25618507'
-API_HASH = 'b8b91983b578360ec05a7e88e17b06ea'
-BOT_TOKEN = '6520550784:AAEUC8Itct_VMe4cbJbUXVZxE-rw8PM0REQ'
-DB_CHANNEL_ID = -1002113326627  # Replace with your DB channel ID
+class Config:
+    # Telegram Bot Token
+    BOT_TOKEN = "6520550784:AAEUC8Itct_VMe4cbJbUXVZxE-rw8PM0REQ"
 
-ADMINS = [6854172577]
+    # Payment Channel
+    PAYMENT_CHANNEL = "@testpostchnl"
+
+    # Owner's User ID
+    OWNER_ID = 6854172577
+
+    # Channels to be checked
+    CHANNELS = ["@testpostchnl"]
+
+    # Daily Bonus amount
+    DAILY_BONUS = 1
+
+    # Minimum Withdraw amount
+    MINI_WITHDRAW = 0.5
+
+    # Per Refer bonus amount
+    PER_REFER = 0.0001
+
+    # Other configuration variables...
+
+# Instantiate the Config class
+config = Config()
+
+
+
+# config.py
+#API_ID = '25618507'
+#API_HASH = 'b8b91983b578360ec05a7e88e17b06ea'
+#BOT_TOKEN = '6520550784:AAEUC8Itct_VMe4cbJbUXVZxE-rw8PM0REQ'
+#DB_CHANNEL_ID = -1002113326627  # Replace with your DB channel ID
+
+#ADMINS = [6854172577]
